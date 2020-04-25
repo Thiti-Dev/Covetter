6ca826f835776fcecfe9e3c769408a5646f98e63
BlueBearrii <thananan.math.acs@gmail.com>
2020-04-26 04:16:32 +0700

    :lipstick::art: Improving and custom styles for register

--- client/src/components/register/Styles.js
+++ client/src/components/register/Styles.js
@@ -4,12 +4,19 @@ const styles = StyleSheet.create({
   header: {
     backgroundColor: '#ededed',
     paddingHorizontal: '5%',
-    paddingVertical: '5%',
+    paddingVertical: '2.5%',
+    flexDirection: 'row',
+    alignItems: 'center',
   },
 
   headerText: {
     fontSize: 18,
     color: '#3d3d3d',
+    marginHorizontal: '5%',
+  },
+
+  buttonStyle: {
+    width: '20%',
   },
 });
 
