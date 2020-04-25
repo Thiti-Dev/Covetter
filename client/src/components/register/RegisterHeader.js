fa8d43ea33108b5fe77cd85c11a05f27de911927
BlueBearrii <thananan.math.acs@gmail.com>
2020-04-26 04:14:54 +0700

    :construction::rotating_light: Improving code of register header file

--- client/src/components/register/RegisterHeader.js
+++ client/src/components/register/RegisterHeader.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 import styles from './Styles';
+import {Button} from '@ant-design/react-native';
 
 const RegisterHeader = () => {
   return (
     <View style={styles.header}>
-      <Text style={styles.headerText}>Header</Text>
+      <Button style={styles.buttonStyle}>Back</Button>
+      <Text style={styles.headerText}>Register</Text>
     </View>
   );
 };
