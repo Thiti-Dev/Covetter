e30d2f63d6fe65080b96a7eea7e434e795181d63
BlueBearrii <thananan.math.acs@gmail.com>
2020-04-26 03:55:14 +0700

    :package: Added Ant Design Mobile RN of React library

---
+++ client/src/components/register/RegisterHeader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import styles from './Styles';
+
+const RegisterHeader = () => {
+  return (
+    <View style={styles.header}>
+      <Text style={styles.headerText}>Header</Text>
+    </View>
+  );
+};
+
+export default RegisterHeader;
---
+++ client/src/components/register/Styles.js
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: '#ededed',
+    paddingHorizontal: '5%',
+    paddingVertical: '5%',
+  },
+
+  headerText: {
+    fontSize: 18,
+    color: '#3d3d3d',
+  },
+});
+
+export default styles;
