6a7cd98005e05969930221400a94342938bedbd6
BlueBearrii <thananan.math.acs@gmail.com>
2020-04-26 23:28:13 +0700

    :page_facing_up: Create register second screen :construction::lipstick: During update

---
+++ client/src/components/register/secondScreen/RegisterInput.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {View, Text, TextInput} from 'react-native';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faUserAlt, faPhone} from '@fortawesome/free-solid-svg-icons';
+import styles from './Styles';
+import {Button} from '@ant-design/react-native';
+
+const formInputDetail = [
+  {key: 'firstName', iconName: faUserAlt, placeholder: 'Firstname'},
+  {key: 'lastName', iconName: faUserAlt, placeholder: 'Lastname'},
+  {key: 'PhoneNumber', iconName: faPhone, placeholder: 'Phone number'},
+];
+
+const mapFormInputDetail = formInputDetail.map(res => {
+  return (
+    <View style={styles.inputForm} key={`key-id-${res.key}`}>
+      <FontAwesomeIcon style={styles.iconInput} size={30} icon={res.iconName} />
+      <TextInput style={styles.textInput} placeholder={res.placeholder} />
+    </View>
+  );
+});
+
+const RegisterInput = () => {
+  return (
+    <View style={styles.input}>
+      {mapFormInputDetail}
+      <View style={styles.buttonFormInput}>
+        <Button style={styles.buttonInput}>
+          <Text style={styles.buttonTextInput}>Create account</Text>
+        </Button>
+      </View>
+    </View>
+  );
+};
+
+export default RegisterInput;
