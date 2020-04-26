4ed2525ec994512703614eb27c0e606f1a4c32d1
BlueBearrii <thananan.math.acs@gmail.com>
2020-04-27 00:55:56 +0700

    :page_facing_up: Created Register agreement overlay

---
+++ client/src/components/register/secondScreen/RegisterAgreement.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import styles from './Styles';
+
+import {Button, CheckBox} from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+const RegisterAgreement = (isCheck, setCheck, setVisibleOverlay) => {
+  return (
+    <View>
+      <Text>Hello from Overlay!</Text>
+      <View style={styles.checkboxViewStyle}>
+        <CheckBox
+          center
+          containerStyle={styles.checkboxStyle}
+          title="Agreement & Terms"
+          checkedIcon="dot-circle-o"
+          uncheckedIcon="circle-o"
+          checked={isCheck}
+          onPress={() => {
+            setCheck(true);
+          }}
+        />
+      </View>
+      <View style={styles.formButtonInOverlalyStyle}>
+        <Button
+          buttonStyle={styles.buttonInOverlay}
+          icon={<Icon name="arrow-right" size={15} color="white" />}
+          title="Submit "
+          iconRight
+          onPress={() => setVisibleOverlay(false)}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default RegisterAgreement;
