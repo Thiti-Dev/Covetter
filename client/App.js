8ed7739467d7d69b201a9aabaaa649563a64b12c
BlueBearrii <thananan.math.acs@gmail.com>
2020-04-26 23:26:22 +0700

    :page_facing_up: Create flow navigation

--- client/App.js
+++ client/App.js
@@ -1,13 +1,54 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
-import Register from './src/components/register/';
+import {NavigationContainer} from '@react-navigation/native';
+import {
+  createStackNavigator,
+  TransitionPresets,
+  CardStyleInterpolators,
+} from '@react-navigation/stack';
+import Register from './src/components/register/firstScreen';
+import RegisterII from './src/components/register/secondScreen';
+
+const Stack = createStackNavigator();
+
+const config = {
+  animation: 'spring',
+  config: {
+    stiffness: 1000,
+    damping: 500,
+    mass: 3,
+    overshootClamping: true,
+    restDisplacementThreshold: 0.01,
+    restSpeedThreshold: 0.01,
+  },
+};
 
 export default class App extends Component {
   render() {
     return (
-      <View>
-        <Register />
-      </View>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="RegisterFirstScreen"
+          screenOptions={{
+            gestureEnabled: true,
+            gestureDirection: 'horizontal',
+            cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+            transitionSpec: {
+              open: config,
+              close: config,
+            },
+          }}>
+          <Stack.Screen
+            name="RegisterFirstScreen"
+            component={Register}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="RegisterSeccondScreen"
+            component={RegisterII}
+            options={{headerShown: false}}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     );
   }
 }
