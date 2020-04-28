56b3f4ef5dad7dbe580d3115c98db8cc37120c43
Thiti Dev <thiti.mwk.main@gmail.com>
2020-04-28 20:36:55 +0700

    [update]: Applied validator to auth/register

--- controller/auth.js
+++ controller/auth.js
@@ -1,13 +1,23 @@
 const admin = require('../utils/firebase/firebase-service');
 const asyncHandler = require('../middleware/async');
+const ErrorResponse = require('../utils/errorResponse');
+
+//
+// ─── VALIDATION ─────────────────────────────────────────────────────────────────
+//
+const validateRegister = require('../utils/validations/register');
+// ────────────────────────────────────────────────────────────────────────────────
+
 exports.register = asyncHandler(async (req, res, next) => {
 	const { email, password, confirmPassword } = req.body;
+	const { isError, errors } = validateRegister(req.body);
 
+	if (isError) {
+		return next(new ErrorResponse(`Validation Error`, 400, errors));
+	}
 	const user = await admin.auth().createUser({
 		email,
-		password,
-		confirmPassword
+		password
 	});
-
 	res.status(201).json({ sucess: true, data: user });
 });
