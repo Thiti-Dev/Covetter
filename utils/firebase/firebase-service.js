8b5b01f3c7e3dd6fe3a8c4be03b0bc5f4c1734e8
Thiti Dev <thiti.mwk.main@gmail.com>
2020-05-01 14:05:21 +0700

    :arrows_clockwise: [parse][env]: Load serviceAccount from proc.env instead of json file

--- utils/firebase/firebase-service.js
+++ utils/firebase/firebase-service.js
@@ -1,5 +1,6 @@
 var admin = require('firebase-admin');
-var serviceAccount = require('../../config/covetter-service-firebase.json');
+//var serviceAccount = require('../../config/covetter-service-firebase.json');
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_JSON_STRINGIFY);
 admin.initializeApp({
 	credential: admin.credential.cert(serviceAccount),
 	databaseURL: 'https://covetter-46323.firebaseio.com/'
