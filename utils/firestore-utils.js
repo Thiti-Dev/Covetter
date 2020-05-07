9a6df2e1977861efe705e036f8c2f10be5484086
Thiti Dev <thiti.mwk.main@gmail.com>
2020-05-07 23:46:33 +0700

    :gem: [firestore-util]: getDataFromCollection (util-Class included)

--- utils/firestore-utils.js
+++ utils/firestore-utils.js
@@ -1,6 +1,33 @@
 const admin = require('../utils/firebase/firebase-service');
 const db = admin.firestore();
 
+//
+// ─── CLASSES UTILS ──────────────────────────────────────────────────────────────
+//
+class Gdfc {
+	constructor(_data = null) {
+		this.empty = _data.empty;
+		if (!this.empty) {
+			this.records = _data.docs.map((doc) => {
+				const docData = doc.data();
+				docData.id = doc.id; // assign the id of every docs
+				return docData;
+			});
+		}
+	}
+
+	//Getting the first element of datas
+	first() {
+		return this.records ? this.records[0] : null;
+	}
+
+	// Getting the records length
+	get length() {
+		return this.records ? this.records.length : 0;
+	}
+}
+// ────────────────────────────────────────────────────────────────────────────────
+
 exports.addMultipleDocFromData = async (data, collection_name) => {
 	let batch = db.batch(); // creating a new batch for executing the multiple tasks
 	data.forEach((doc) => {
@@ -20,4 +47,24 @@ exports.getAllDataFromCollection = async (collection_name) => {
 		return docData;
 	});
 	return _data;
+};
+
+exports.getDataFromCollection = async (collection_name, option) => {
+	let collectionRef = db.collection(collection_name);
+	// executing each option has passed
+	if (option) {
+		if (option.limit) {
+			collectionRef = collectionRef.limit(option.limit);
+		}
+		if (option.query) {
+			//Extracting value
+			const { key, operator, matchValue } = option.query;
+			collectionRef = collectionRef.where(key, operator, matchValue);
+		}
+	}
+
+	collectionRef = collectionRef.get();
+
+	const _res = await collectionRef;
+	return new Gdfc(_res);
 };
