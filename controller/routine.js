cd36b1e8e2b85f8d08b7ce364a1394ec7da0f5aa
Thiti Dev <thiti.mwk.main@gmail.com>
2020-05-08 00:02:06 +0700

    :zap: [routine-controller]: Created answerRoutineQuiz

--- controller/routine.js
+++ controller/routine.js
@@ -14,7 +14,7 @@ const db = admin.firestore();
 //
 // ─── FIRESTORE UTILS ────────────────────────────────────────────────────────────
 //
-const { getAllDataFromCollection } = require('../utils/firestore-utils');
+const { getAllDataFromCollection, getDataFromCollection } = require('../utils/firestore-utils');
 // ────────────────────────────────────────────────────────────────────────────────
 
 //
@@ -27,7 +27,35 @@ const { getAllDataFromCollection } = require('../utils/firestore-utils');
 // TYPE 3 === INPUT WHEELS [ need extra field to strore the option]
 // ────────────────────────────────────────────────────────────────────────────────
 
+// @desc    Get all of the routine quizes in the firestore database
+// @route   GET /api/routine
+// @acess   Public
 exports.getAllRoutineQuiz = asyncHandler(async (req, res, next) => {
 	const quizes = await getAllDataFromCollection('routine_quiz');
 	res.status(200).json({ sucess: true, data: quizes });
+});
+
+// @desc    Answer the routine quiz
+// @route   POST /api/routine/:quiz_id/answer
+// @acess   Private
+exports.answerRoutineQuiz = asyncHandler(async (req, res, next) => {
+	const { quiz_id } = req.params;
+	const { answer } = req.body;
+	let quiz_data;
+	//const _res = await db.collection('routine_quiz').where('quiz_id', '==', quiz_id).limit(1).get();
+	const _data = await getDataFromCollection('routine_quiz', {
+		limit: 1,
+		query: { key: 'quiz_id', operator: '==', matchValue: quiz_id }
+	});
+	if (_data.empty) {
+		return next(new ErrorResponse(`The quiz with id:${quiz_id} isn't exist`, 404));
+	}
+	quiz_data = _data.first();
+
+	const _res = await db.collection('users').doc(req.user).collection('routine_answers').add({
+		quiz_id: quiz_id,
+		quiz_answer: answer
+	});
+
+	res.status(200).json({ sucess: true, data: _res.id });
 });
--- routes/api/routine.js
+++ routes/api/routine.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAllRoutineQuiz } = require('../../controller/routine');
+const { getAllRoutineQuiz, answerRoutineQuiz } = require('../../controller/routine');
 
 const { protect } = require('../../middleware/auth');
 
 router.route('/').get(getAllRoutineQuiz);
+router.route('/:quiz_id/answer').post(protect, answerRoutineQuiz);
 module.exports = router;
