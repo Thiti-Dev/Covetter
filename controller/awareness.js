0ecc8e1c717711f814dbc6cc443b5cdc92af695c
Thiti Dev <thiti.mwk.main@gmail.com>
2020-05-08 21:50:53 +0700

    :zap: [awareness-controller]: Created commitNewAwarenessData

--- controller/awareness.js
+++ controller/awareness.js
@@ -18,7 +18,22 @@ const FieldValue = admin.firestore.FieldValue;
 const { getAllDataFromCollection } = require('../utils/firestore-utils');
 // ────────────────────────────────────────────────────────────────────────────────
 
+//
+// ─── UTIL ───────────────────────────────────────────────────────────────────────
+//
+let objKeyFilter = require('../utils/objectKeyFilter');
+// ────────────────────────────────────────────────────────────────────────────────
+
 exports.getAllAwarenessLocationAndInfo = asyncHandler(async (req, res, next) => {
 	const awareness_data = await getAllDataFromCollection('awareness_data');
 	res.status(200).json({ sucess: true, data: awareness_data });
+});
+
+exports.commitNewAwarenessData = asyncHandler(async (req, res, next) => {
+	const staged_awareness_data = objKeyFilter(req.body, [ 'reason', 'involved', 'position' ]);
+	const _res = await db.collection('awareness_data').add({
+		...staged_awareness_data,
+		createdAt: FieldValue.serverTimestamp()
+	});
+	res.status(200).json({ sucess: true, data: _res.id });
 });
--- routes/api/awareness.js
+++ routes/api/awareness.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAllAwarenessLocationAndInfo } = require('../../controller/awareness');
+const { getAllAwarenessLocationAndInfo, commitNewAwarenessData } = require('../../controller/awareness');
 
 const { protect } = require('../../middleware/auth');
 
-router.route('/').get(getAllAwarenessLocationAndInfo);
+router.route('/').get(getAllAwarenessLocationAndInfo).post(protect, commitNewAwarenessData);
 module.exports = router;
