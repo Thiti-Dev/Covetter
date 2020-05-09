2b6b915a09b05ccd71655fae86decccebb89cec1
Thiti Dev <thiti.mwk.main@gmail.com>
2020-05-10 03:31:53 +0700

    :wrench: [storage-bucket]: Initalized

--- utils/firebase/firebase-service.js
+++ utils/firebase/firebase-service.js
@@ -3,7 +3,8 @@ var admin = require('firebase-admin');
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_JSON_STRINGIFY);
 admin.initializeApp({
 	credential: admin.credential.cert(serviceAccount),
-	databaseURL: 'https://covetter-46323.firebaseio.com/'
+	databaseURL: 'https://covetter-46323.firebaseio.com/',
+	storageBucket: 'gs://covetter-46323.appspot.com'
 });
 
 module.exports = admin;
