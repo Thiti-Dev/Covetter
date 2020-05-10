7f2dfcd740baedb832f30ffc3d5934424e5da506
Thiti Dev <thiti.mwk.main@gmail.com>
2020-05-10 20:48:02 +0700

    :zap: [succor-controller]: Created commitCharity

--- controller/succor.js
+++ controller/succor.js
@@ -15,7 +15,21 @@ const FieldValue = admin.firestore.FieldValue;
 //
 // ─── FIRESTORE UTILS ────────────────────────────────────────────────────────────
 //
-const { getAllDataFromCollection } = require('../utils/firestore-utils');
+const { getAllDataFromCollection, uploadPhotoToStorage } = require('../utils/firestore-utils');
+// ────────────────────────────────────────────────────────────────────────────────
+
+//
+// ─── GEOFIRESTORE ( FOR FINDING THE NEAREST LOCATION FROM POINT) ────────────────
+//
+var GeoFirestore = require('geofirestore');
+const geofirestore = new GeoFirestore.GeoFirestore(db);
+const geocollection = geofirestore.collection('succor');
+// ────────────────────────────────────────────────────────────────────────────────
+
+//
+// ─── UTIL ───────────────────────────────────────────────────────────────────────
+//
+const geocoder = require('../utils/geocoder');
 // ────────────────────────────────────────────────────────────────────────────────
 
 // @desc    Get all of the supoorted location data
@@ -24,4 +38,43 @@ const { getAllDataFromCollection } = require('../utils/firestore-utils');
 exports.getAllSupportedData = asyncHandler(async (req, res, next) => {
 	const awareness_data = await getAllDataFromCollection('succor');
 	res.status(200).json({ sucess: true, data: awareness_data });
+});
+
+// @desc    Committing the charity/succor
+// @route   POST /api/succor/
+// @acess   Private
+exports.commitCharity = asyncHandler(async (req, res, next) => {
+	//
+	// ─── CHECK FOR FILE UPLOAD FIRST ─────────────────────────────────────────────────────────
+	//
+
+	if (!req.file) {
+		return next(new ErrorResponse(`Please upload a file`, 400));
+	}
+
+	const file = req.file;
+
+	// Make sure the image is a photo
+	if (!file.mimetype.startsWith('image')) {
+		return next(new ErrorResponse(`Please upload an image file`, 400));
+	}
+	// ────────────────────────────────────────────────────────────────────────────────
+	const _upload_url = await uploadPhotoToStorage(req.file, 'images');
+
+	const lat = parseInt(req.body.lat);
+	const lng = parseInt(req.body.lng);
+
+	const geo_res = await geocoder.reverse({
+		lat: lat,
+		lon: lng
+	});
+	const _res = await geocollection.add({
+		description: req.body.description,
+		address: geo_res[0].formattedAddress,
+		photo_url: _upload_url,
+		coordinates: new admin.firestore.GeoPoint(lat, lng),
+		createdAt: FieldValue.serverTimestamp(),
+		endAt: admin.firestore.Timestamp.fromDate(new Date(req.body.endAt))
+	});
+	res.status(200).json({ sucess: true, data: _res.id });
 });
--- routes/api/succor.js
+++ routes/api/succor.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAllSupportedData } = require('../../controller/succor');
+const { getAllSupportedData, commitCharity } = require('../../controller/succor');
 
 const { protect } = require('../../middleware/auth');
 
-router.route('/').get(getAllSupportedData);
+const multer = require('multer');
+const upload = multer({
+	storage: multer.memoryStorage(),
+	limits: {
+		fileSize: 5 * 1024 * 1024 // no larger than 5mb, you can change as needed.
+	}
+});
+
+// Use any() for an dynamic specification
+router.route('/').get(getAllSupportedData).post(protect, upload.single('file'), commitCharity);
 module.exports = router;
