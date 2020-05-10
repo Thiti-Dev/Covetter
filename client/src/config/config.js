3e5e8533f1724f641c104e32e08e3ac58d9eb32d
BlueBearrii <thananan.math.acs@gmail.com>
2020-05-11 05:02:29 +0700

    [Config] firebase config initiallization

---
+++ client/src/config/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAJ3FE_cAbGUJs6QkdZ-04jjgn9H7y0w9c',
+  authDomain: 'covetter-46323.firebaseapp.com',
+  databaseURL: 'https://covetter-46323.firebaseio.com',
+  projectId: 'covetter-46323',
+  storageBucket: 'covetter-46323.appspot.com',
+  messagingSenderId: '319270902210',
+  appId: '1:319270902210:web:1a74b9107f46ea03af5e5b',
+  measurementId: 'G-D4R920D34W',
+};
+
+export default firebaseConfig;
