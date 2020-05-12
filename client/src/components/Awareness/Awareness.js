5407cbeb8068c40f9d077fc04e7a97aa7137e670
Thiti Dev <thiti.mwk.main@gmail.com>
2020-05-13 01:22:15 +0700

    :gem: [update][awareness-component]: Commit the awareness situation

--- client/src/components/Awareness/Awareness.js
+++ client/src/components/Awareness/Awareness.js
@@ -1,12 +1,7 @@
-import React, {Component} from 'react';
-import {Text, View, PermissionsAndroid, ScrollView} from 'react-native';
-import {Flex, WhiteSpace} from '@ant-design/react-native';
-import MapView, {
-  Marker,
-  AnimatedRegion,
-  Animated,
-  Circle,
-} from 'react-native-maps';
+import React, { Component } from 'react';
+import { Text, View, PermissionsAndroid, ScrollView } from 'react-native';
+import { Flex, WhiteSpace } from '@ant-design/react-native';
+import MapView, { Marker, AnimatedRegion, Animated, Circle } from 'react-native-maps';
 import axios from 'axios';
 import Geolocation from 'react-native-geolocation-service';
 
@@ -16,263 +11,314 @@ import Geolocation from 'react-native-geolocation-service';
 import distance from '../../utils/distance';
 import AddEventButton from './AddEventButton';
 import ModalPop from './ModalPop';
-import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
-import {faMapMarkerAlt} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 // ────────────────────────────────────────────────────────────────────────────────
 
 export default class Awareness extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      awareness_locations: null,
-      userLatitude: 0,
-      userLogintude: 0,
-      initialized: false,
-      modalVisible: false,
-    };
-  }
-  async getAllAwarenessData(lat, lng) {
-    try {
-      const _res = await axios.post(
-        'https://covetter-api.herokuapp.com/api/awareness/nearest',
-        {lat, lng},
-      );
-      const all_awareness_data = _res.data.data;
-      this.setState({
-        awareness_locations: all_awareness_data,
-        initialized: true,
-      });
-    } catch (error) {}
-  }
-  async onMapLoaded() {
-    //
-    // ─── ASKING FOR PERMISSION ───────────────────────────────────────
-    //
-    let use_loc = false;
-    let granted = await PermissionsAndroid.request(
-      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-      {
-        title: 'App Geolocation Permission',
-        message: "App needs access to your phone's location.",
-      },
-    );
+	constructor(props) {
+		super(props);
+		this.state = {
+			awareness_locations: null,
+			userLatitude: 0,
+			userLongitude: 0,
+			initialized: false,
+			modalVisible: false,
+			statusBarHeight: 0
+		};
+	}
+	async getAllAwarenessData(lat, lng) {
+		try {
+			const _res = await axios.post('https://covetter-api.herokuapp.com/api/awareness/nearest', { lat, lng });
+			const all_awareness_data = _res.data.data;
+			this.setState({
+				awareness_locations: all_awareness_data,
+				initialized: true
+			});
+		} catch (error) {}
+	}
+	async onMapLoaded() {
+		//
+		// ─── ASKING FOR PERMISSION ───────────────────────────────────────
+		//
+		let use_loc = false;
+		let granted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION, {
+			title: 'App Geolocation Permission',
+			message: "App needs access to your phone's location."
+		});
 
-    if (granted) {
-      use_loc = true;
-      console.log('Location permission granted!');
-    } else {
-      console.log('Location permission not granted!!!!');
-    }
-    // ─────────────────────────────────────────────────────────────────
+		if (granted) {
+			use_loc = true;
+			console.log('Location permission granted!');
+		} else {
+			console.log('Location permission not granted!!!!');
+		}
+		// ─────────────────────────────────────────────────────────────────
 
-    if (use_loc) {
-      Geolocation.getCurrentPosition(
-        position => {
-          this.setState({
-            userLatitude: position.coords.latitude,
-            userLogintude: position.coords.longitude,
-          });
-          if (!this.state.initialized) {
-            this.getAllAwarenessData(
-              position.coords.latitude,
-              position.coords.longitude,
-            );
-          }
-        },
-        error => {
-          // See error code charts below.
-          console.log(error.code, error.message);
-        },
-        {enableHighAccuracy: true, timeout: 15000},
-      );
-    }
-  }
-  componentDidMount() {
-    this.locationWatchId = Geolocation.watchPosition(
-      position => {
-        this.setState({
-          userLatitude: position.coords.latitude,
-          userLogintude: position.coords.longitude,
-        });
-        //console.log(position.coords);
-      },
-      error => {
-        // See error code charts below.
-        console.warn(error.code, error.message);
-      },
-      {
-        enableHighAccuracy: true,
-        timeout: 15000,
-        maximumAge: 10000,
-        interval: 1000,
-        distanceFilter: 1,
-        fastestInterval: 1000,
-      },
-    );
-    //this.getAllAwarenessData();
-  }
-  componentWillUnmount = () => {
-    Geolocation.clearWatch(this.locationWatchId);
-  };
+		if (use_loc) {
+			Geolocation.getCurrentPosition(
+				(position) => {
+					this.setState({
+						userLatitude: position.coords.latitude,
+						userLongitude: position.coords.longitude
+					});
+					if (!this.state.initialized) {
+						this.getAllAwarenessData(position.coords.latitude, position.coords.longitude);
+					}
+				},
+				(error) => {
+					// See error code charts below.
+					console.log(error.code, error.message);
+				},
+				{ enableHighAccuracy: true, timeout: 15000 }
+			);
+		}
+		setTimeout(() => {
+			this.followUserLocation();
+		}, 2000);
+	}
+	componentDidMount() {
+		// Fixed showMyLocationButton for react-native-maps problem on some devices
+		setTimeout(() => this.setState({ statusBarHeight: 2 }), 500); // DO NOT DELETE
+		// ─────────────────────────────────────────────────────────────────
 
-  render() {
-    console.log('Worked');
-    const setModalVisible = visible => {
-      this.setState({modalVisible: visible});
-    };
-    const {awareness_locations, userLatitude, userLogintude} = this.state;
-    let render_awareness_location, map_rendered_circle;
-    if (awareness_locations) {
-      render_awareness_location = awareness_locations.map((data, key) => {
-        return (
-          <View
-            key={key}
-            style={{
-              backgroundColor: '#ffffff',
-              minHeight: 100,
-              padding: 10,
-              marginBottom: 10,
-              borderRadius: 15,
-              justifyContent: 'center',
-              shadowColor: '#000',
-              shadowOffset: {
-                width: 0,
-                height: 1,
-              },
-              shadowOpacity: 0.22,
-              shadowRadius: 2.22,
+		//
+		// ─── REGISTERING POSITION WATCHER───────────────────────────────────────
+		//
+		this.locationWatchId = Geolocation.watchPosition(
+			(position) => {
+				this.setState({
+					userLatitude: position.coords.latitude,
+					userLongitude: position.coords.longitude
+				});
+				//console.log(position.coords);
+			},
+			(error) => {
+				// See error code charts below.
+				console.warn(error.code, error.message);
+			},
+			{
+				enableHighAccuracy: true,
+				timeout: 15000,
+				maximumAge: 10000,
+				interval: 1000,
+				distanceFilter: 1,
+				fastestInterval: 1000
+			}
+		);
+		// ─────────────────────────────────────────────────────────────────
 
-              elevation: 3,
-            }}>
-            <View>
-              <Text
-                style={{
-                  fontFamily: 'Prompt-Bold',
-                  fontSize: 16,
-                  color: '#475055',
-                }}>
-                📣 {data.reason}
-              </Text>
-              <View
-                style={{
-                  flexDirection: 'row',
-                  justifyContent: 'flex-start',
-                  alignItems: 'center',
-                  marginTop: 10,
-                }}>
-                <FontAwesomeIcon
-                  icon={faMapMarkerAlt}
-                  color="#e84848"
-                  style={{marginHorizontal: 10}}
-                  size={20}
-                />
-                <Text
-                  style={{
-                    fontFamily: 'Prompt-Regular',
-                    fontSize: 16,
-                    color: '#475055',
-                  }}>
-                  {distance(
-                    userLatitude,
-                    userLogintude,
-                    data.position.lat,
-                    data.position.lng,
-                    'K',
-                  )}
-                  {` Kilometer.`}
-                </Text>
-              </View>
-            </View>
-          </View>
-        );
-      });
-      map_rendered_circle = awareness_locations.map((data, key) => {
-        return (
-          <Circle
-            key={key}
-            center={{latitude: data.position.lat, longitude: data.position.lng}}
-            radius={25}
-            strokeColor={'red'}
-            fillColor={'rgba(255,0,0,0.3)'}
-          />
-        );
-      });
-    }
-    return (
-      <View style={{flex: 1, backgroundColor: '#ffffff'}}>
-        <React.Fragment>
-          <View
-            style={{
-              padding: 10,
-              backgroundColor: '#ffffff',
-              shadowColor: '#000',
-              shadowOffset: {
-                width: 0,
-                height: 1,
-              },
-              shadowOpacity: 0.22,
-              shadowRadius: 2.22,
+		//
+		// ─── SUBSCRIBING FOR NAVIGATION ─────────────────────────────────────
+		//
+		this.focusListener = this.props.navigation.addListener('focus', () => {
+			this.followUserLocation();
+		});
+		// ─────────────────────────────────────────────────────────────────
+	}
+	componentWillUnmount = () => {
+		Geolocation.clearWatch(this.locationWatchId);
+		this.focusListener(); // clearing the event
+	};
 
-              elevation: 5,
-            }}>
-            <Text
-              style={{
-                fontFamily: 'Prompt-Bold',
-                fontSize: 20,
-                color: '#475055',
-              }}>
-              🚀 Awareness Scanner
-            </Text>
-          </View>
-          <View
-            style={{
-              height: '30%',
-              backgroundColor: '#fff',
-              shadowColor: '#000',
-              shadowOffset: {
-                width: 0,
-                height: 1,
-              },
-              shadowOpacity: 0.22,
-              shadowRadius: 2.22,
+	//
+	// ─── FREQ USED ──────────────────────────────────────────────────────────────────
+	//
+	followUserLocation() {
+		const { userLatitude, userLongitude } = this.state;
+		this.map.animateToRegion(
+			{
+				longitude: userLongitude,
+				latitude: userLatitude,
+				longitudeDelta: 0.002,
+				latitudeDelta: 0.002
+			},
+			1000
+		);
+	}
+	// ────────────────────────────────────────────────────────────────────────────────
 
-              elevation: 1.5,
-            }}>
-            <MapView
-              style={{flex: 1}}
-              provider={'google'}
-              ref={map => {
-                // Referencing the map
-                this.map = map;
-              }}
-              track
-              initialRegion={{
-                latitude: 13.652944,
-                longitude: 100.49525,
-                latitudeDelta: 0.002,
-                longitudeDelta: 0.002,
-              }}
-              onMapReady={this.onMapLoaded.bind(this)}
-              showsUserLocation={true}>
-              {map_rendered_circle}
-            </MapView>
-          </View>
-          <ScrollView>
-            <View
-              style={{height: '50%', paddingHorizontal: 10, paddingTop: 10}}>
-              {render_awareness_location}
-            </View>
-          </ScrollView>
-          <AddEventButton
-            modalVisible={this.state.modalVisible}
-            setIsVisible={setModalVisible}
-          />
-          <ModalPop
-            modalVisible={this.state.modalVisible}
-            setIsVisible={setModalVisible}
-          />
-        </React.Fragment>
-      </View>
-    );
-  }
+	//
+	// ─── CALLBACK ───────────────────────────────────────────────────────────────────
+	//
+	onCommitSuccess() {
+		const { userLatitude, userLongitude } = this.state;
+		// On commit success => fetch new nearest awareness data
+		this.getAllAwarenessData(userLatitude, userLongitude);
+		this.setModalVisible(false);
+	}
+	setModalVisible(visible) {
+		this.setState({ modalVisible: visible });
+	}
+	// ────────────────────────────────────────────────────────────────────────────────
+
+	render() {
+		const { awareness_locations, userLatitude, userLongitude } = this.state;
+		let render_awareness_location, map_rendered_circle;
+		if (awareness_locations) {
+			render_awareness_location = awareness_locations.map((data, key) => {
+				return (
+					<View
+						key={key}
+						style={{
+							backgroundColor: '#ffffff',
+							minHeight: 100,
+							padding: 10,
+							marginBottom: 10,
+							borderRadius: 15,
+							justifyContent: 'center',
+							shadowColor: '#000',
+							shadowOffset: {
+								width: 0,
+								height: 1
+							},
+							shadowOpacity: 0.22,
+							shadowRadius: 2.22,
+
+							elevation: 3
+						}}
+					>
+						<View>
+							<Text
+								style={{
+									fontFamily: 'Prompt-Bold',
+									fontSize: 16,
+									color: '#475055'
+								}}
+							>
+								📣 {data.reason}
+							</Text>
+							<View
+								style={{
+									flexDirection: 'row',
+									justifyContent: 'flex-start',
+									alignItems: 'center',
+									marginTop: 10
+								}}
+							>
+								<FontAwesomeIcon
+									icon={faMapMarkerAlt}
+									color="#e84848"
+									style={{ marginHorizontal: 10 }}
+									size={20}
+								/>
+								<Text
+									style={{
+										fontFamily: 'Prompt-Regular',
+										fontSize: 16,
+										color: '#475055'
+									}}
+								>
+									{distance(
+										userLatitude,
+										userLongitude,
+										data.position.lat,
+										data.position.lng,
+										'K'
+									).toFixed('2')}
+									{` Kilometer.`}
+								</Text>
+							</View>
+						</View>
+					</View>
+				);
+			});
+			map_rendered_circle = awareness_locations.map((data, key) => {
+				return (
+					<Circle
+						key={key}
+						center={{ latitude: data.position.lat, longitude: data.position.lng }}
+						radius={25}
+						strokeColor={'red'}
+						fillColor={'rgba(255,0,0,0.3)'}
+					/>
+				);
+			});
+		}
+		return (
+			<React.Fragment>
+				<View style={{ flex: 1, backgroundColor: '#ffffff' }}>
+					<React.Fragment>
+						<View
+							style={{
+								padding: 10,
+								backgroundColor: '#ffffff',
+								shadowColor: '#000',
+								shadowOffset: {
+									width: 0,
+									height: 1
+								},
+								shadowOpacity: 0.22,
+								shadowRadius: 2.22,
+
+								elevation: 5
+							}}
+						>
+							<Text
+								style={{
+									fontFamily: 'Prompt-Bold',
+									fontSize: 20,
+									color: '#475055'
+								}}
+							>
+								🚀 Awareness Scanner
+							</Text>
+						</View>
+						<View
+							style={{
+								height: '30%',
+								backgroundColor: '#fff',
+								shadowColor: '#000',
+								shadowOffset: {
+									width: 0,
+									height: 1
+								},
+								shadowOpacity: 0.22,
+								shadowRadius: 2.22,
+
+								elevation: 1.5,
+								paddingTop: this.state.statusBarHeight
+							}}
+						>
+							<MapView
+								style={{ flex: 1 }}
+								provider={'google'}
+								ref={(map) => {
+									// Referencing the map
+									this.map = map;
+								}}
+								track
+								initialRegion={{
+									latitude: 13.652944,
+									longitude: 100.49525,
+									latitudeDelta: 0.002,
+									longitudeDelta: 0.002
+								}}
+								onMapReady={this.onMapLoaded.bind(this)}
+								showsUserLocation={true}
+								showsMyLocationButton={true}
+							>
+								{map_rendered_circle}
+							</MapView>
+						</View>
+						<ScrollView>
+							<View style={{ height: '50%', paddingHorizontal: 10, paddingTop: 10 }}>
+								{render_awareness_location}
+							</View>
+						</ScrollView>
+						<AddEventButton
+							modalVisible={this.state.modalVisible}
+							setIsVisible={this.setModalVisible.bind(this)}
+						/>
+						<ModalPop
+							modalVisible={this.state.modalVisible}
+							setIsVisible={this.setModalVisible.bind(this)}
+							user_position={{ lat: userLatitude, lng: userLongitude }}
+							on_success={this.onCommitSuccess.bind(this)}
+						/>
+					</React.Fragment>
+				</View>
+			</React.Fragment>
+		);
+	}
 }
--- client/src/components/Awareness/ModalPop.js
+++ client/src/components/Awareness/ModalPop.js
@@ -1,94 +1,120 @@
-import React, {Component} from 'react';
-import {
-  Text,
-  View,
-  TextInput,
-  TouchableOpacity,
-  Modal,
-  Image,
-} from 'react-native';
+import React, { Component } from 'react';
+import { Text, View, TextInput, TouchableOpacity, Modal, Image } from 'react-native';
+import axios from 'axios';
 
 export default class ModalPop extends Component {
-  render() {
-    return (
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={this.props.modalVisible}>
-        <View
-          style={{
-            backgroundColor: 'rgba(0,0,0,0.5)',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '100%',
-          }}>
-          <View
-            style={{
-              width: '85%',
-              backgroundColor: '#fff',
-              justifyContent: 'center',
-              alignItems: 'center',
-              padding: 10,
-              borderRadius: 10,
-            }}>
-            <TouchableOpacity
-              style={{
-                justifyContent: 'center',
-                alignItems: 'center',
-                alignSelf: 'flex-end',
-              }}
-              onPress={() => this.props.setIsVisible(false)}>
-              <Text style={{padding: 10}}>Close</Text>
-            </TouchableOpacity>
-            <Text style={{padding: 10, fontSize: 24, fontFamily: 'Promt-Bold'}}>
-              📣 แจ้งพิกัดอันตราย
-            </Text>
-            <Image
-              source={require('../../assets/images/blood.png')}
-              style={{height: '40%', width: '40%'}}
-              resizeMode="center"
-            />
-            <TextInput
-              placeholder="Enter"
-              style={{
-                width: '90%',
-                padding: 10,
-                backgroundColor: '#fff',
-                shadowColor: '#000',
-                shadowOffset: {
-                  width: 0,
-                  height: 0,
-                },
-                shadowOpacity: 0.22,
-                shadowRadius: 2.22,
+	constructor(props) {
+		super(props);
+		this.state = {
+			reason: ''
+		};
+		this.onCommitAwareness = this.onCommitAwareness.bind(this);
+	}
+	async onCommitAwareness() {
+		const { reason } = this.state;
+		const { lat, lng } = this.props.user_position;
+		if (lat != 0 && lng != 0) {
+			//Proceed [Location has been sent from parent]
+			const _body = {
+				reason,
+				position: {
+					lat,
+					lng
+				}
+			};
+			try {
+				const commit_res = await axios.post('/api/awareness', _body);
+				// If commit success fully
+				console.log('committed');
+				this.props.on_success(); // re-fetch the location
+			} catch (error) {
+				console.log(error.response.data);
+			}
+		}
+	}
+	render() {
+		const { reason } = this.state;
+		return (
+			<Modal animationType="slide" transparent={true} visible={this.props.modalVisible}>
+				<View
+					style={{
+						backgroundColor: 'rgba(0,0,0,0.5)',
+						justifyContent: 'center',
+						alignItems: 'center',
+						height: '100%'
+					}}
+				>
+					<View
+						style={{
+							width: '85%',
+							backgroundColor: '#fff',
+							justifyContent: 'center',
+							alignItems: 'center',
+							padding: 10,
+							borderRadius: 10
+						}}
+					>
+						<TouchableOpacity
+							style={{
+								justifyContent: 'center',
+								alignItems: 'center',
+								alignSelf: 'flex-end'
+							}}
+							onPress={() => this.props.setIsVisible(false)}
+						>
+							<Text style={{ padding: 10 }}>Close</Text>
+						</TouchableOpacity>
+						<Text style={{ padding: 10, fontSize: 24, fontFamily: 'Promt-Bold' }}>📣 แจ้งพิกัดอันตราย</Text>
+						<Image
+							source={require('../../assets/images/blood.png')}
+							style={{ height: '40%', width: '40%' }}
+							resizeMode="center"
+						/>
+						<TextInput
+							onChangeText={(value) => this.setState({ reason: value })}
+							value={reason}
+							placeholder="Enter"
+							style={{
+								width: '90%',
+								padding: 10,
+								backgroundColor: '#fff',
+								shadowColor: '#000',
+								shadowOffset: {
+									width: 0,
+									height: 0
+								},
+								shadowOpacity: 0.22,
+								shadowRadius: 2.22,
 
-                elevation: 3,
-                borderRadius: 10,
-              }}
-            />
-            <Text
-              style={{margin: 10, fontSize: 14, fontFamily: 'Promt-Regular'}}>
-              ✏️ กรุณาระบุข้อความเกี่ยวกับสถานที่อันตราย
-            </Text>
-            <TouchableOpacity
-              style={{
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}>
-              <Text
-                style={{
-                  padding: 10,
-                  fontSize: 18,
-                  color: '#be4038',
-                  fontFamily: 'Promt-Bold',
-                }}>
-                Save
-              </Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      </Modal>
-    );
-  }
+								elevation: 3,
+								borderRadius: 10
+							}}
+						/>
+						<Text style={{ margin: 10, fontSize: 14, fontFamily: 'Promt-Regular' }}>
+							✏️ กรุณาระบุข้อความเกี่ยวกับสถานที่อันตราย
+						</Text>
+						<TouchableOpacity
+							style={{
+								flex: 1,
+								justifyContent: 'center',
+								alignItems: 'center'
+							}}
+							onPress={this.onCommitAwareness}
+						>
+							<Text
+								style={{
+									padding: 10,
+									fontSize: 18,
+									color: '#be4038',
+									fontFamily: 'Promt-Bold'
+								}}
+							>
+								Save
+							</Text>
+						</TouchableOpacity>
+					</View>
+				</View>
+			</Modal>
+		);
+	}
 }
