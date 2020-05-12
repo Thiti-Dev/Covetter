a96f604296dcfd233bcdfc6414c6c49c9208771a
BlueBearrii <thananan.math.acs@gmail.com>
2020-05-13 03:02:41 +0700

    :zap::lipstick: Routine quizs ui updated

--- client/src/components/quiz/Quiz.js
+++ client/src/components/quiz/Quiz.js
@@ -1,29 +1,30 @@
-import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
 import QuizHeader from './QuizHeader';
 import QuizRoutine from './QuizRoutine';
 import Axios from 'axios';
+import {ScrollView} from 'react-native-gesture-handler';
 
 export default class Quiz extends Component {
-	constructor() {
-		super();
-		this.state = {
-			routine: []
-		};
-	}
+  constructor() {
+    super();
+    this.state = {
+      routine: [],
+    };
+  }
 
-	componentDidMount() {
-		Axios.get('/api/routine').then((response) => {
-			console.log(response.data.data);
-			this.setState({ routine: response.data.data });
-		});
-	}
-	render() {
-		return (
-			<View>
-				<QuizHeader />
-				<QuizRoutine state_routine={this.state.routine} />
-			</View>
-		);
-	}
+  componentDidMount() {
+    Axios.get('/api/routine').then(response => {
+      console.log(response.data.data);
+      this.setState({routine: response.data.data});
+    });
+  }
+  render() {
+    return (
+      <ScrollView style={{backgroundColor: '#fff'}}>
+        <QuizHeader />
+        <QuizRoutine state_routine={this.state.routine} />
+      </ScrollView>
+    );
+  }
 }
