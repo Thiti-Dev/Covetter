ec07d0f3cd0f317aeb209e728ba2a996e7a1e826
Thiti Dev <thiti.mwk.main@gmail.com>
2020-05-12 15:59:57 +0700

    :gem: [profile-component]: Created uploadPhoto From storage/camera

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -8,15 +8,24 @@ import EditProfile from './EditProfile';
 import { ScrollView } from 'react-native-gesture-handler';
 import axios from 'axios';
 import firebase from 'firebase';
+
+import ImagePicker from 'react-native-image-picker';
+
+//
+// ─── UTIL ───────────────────────────────────────────────────────────────────────
+//
+import createFormData from '../../utils/createFormData';
+// ────────────────────────────────────────────────────────────────────────────────
+
 export default class Profile extends Component {
 	constructor() {
 		super();
 		this.state = {
 			toggleState: false,
 			user_profile: {
-				name: 'admin',
-				email: 'admin@mail.com',
-				phone: '0991231213',
+				name: '. . .',
+				email: '. . .',
+				phone: '. . .',
 				photo_url: ''
 			},
 			staged_edit_data: {
@@ -26,6 +35,7 @@ export default class Profile extends Component {
 		};
 		this.onEditCredential = this.onEditCredential.bind(this);
 		this.onEditHandler = this.onEditHandler.bind(this);
+		this.onSelectImageFromLibrary = this.onSelectImageFromLibrary.bind(this);
 	}
 	async fetchUserCredential() {
 		try {
@@ -47,8 +57,14 @@ export default class Profile extends Component {
 				name: profile_data.firstName + ' ' + profile_data.lastName,
 				phone: profile_data.phone
 			};
-			this.setState({ user_profile: finalized_profile_data, staged_edit_data: _staged_edit });
-		} catch (error) {}
+			this.setState({
+				user_profile: finalized_profile_data,
+				staged_edit_data: _staged_edit
+			});
+		} catch (error) {
+			// If failed to load proifle
+			// @TODO show an error / infinite spinning
+		}
 	}
 
 	async onEditHandler() {
@@ -104,6 +120,78 @@ export default class Profile extends Component {
 		}
 	}
 
+	async onSelectImageFromLibrary() {
+		console.log('selecting image');
+		const options = {
+			title: 'Select Avatar',
+			//customButtons: [ { name: 'fb', title: 'Choose Photo from Facebook' } ],
+			storageOptions: {
+				skipBackup: true,
+				path: 'images'
+			}
+		};
+		ImagePicker.showImagePicker(options, async (response) => {
+			//console.log('Response = ', response.data);
+
+			if (response.didCancel) {
+				console.log('User cancelled image picker');
+			} else if (response.error) {
+				console.log('ImagePicker Error: ', response.error);
+			} else if (response.customButton) {
+				//console.log('User tapped custom button: ', response.customButton);
+			} else {
+				/*
+				// A method for using fetch ( Example by Thiti Mahawannakit )
+				// Hard debugging
+				let formData = new FormData();
+				const _image = {
+					name: response.fileName,
+					type: response.type,
+					path: response.path,
+					uri: response.uri
+				};
+				formData.append('file', _image);
+				
+				const userToken = await firebase.auth().currentUser.getIdToken();
+				fetch('http://192.168.1.35:5000/api/auth/upload_photo', {
+					method: 'PUT',
+					headers: {
+						Accept: 'application/json',
+						'Content-Type': 'multipart/form-data',
+						Authorization: userToken
+					},
+					body: formData
+				})
+					.then((response) => console.log(response.json()))
+					.catch((err) => console.log(err));
+				*/
+
+				// Creating the from data from a response
+				// FLIPPER MUST BE VERSION 0.41.0 WITH REACT 0.62 ( IF NOT THIS REQUEST METHOD WILL THROW AN NETWORK ERROR ) ( use me like 6 hrs to findout that it was react falut lol)
+				// Problem solved by [Thiti Mahawannakit ]
+				const formData = createFormData(response);
+				try {
+					const _upload_res = await axios.put('/api/auth/upload_photo', formData);
+					// If uploading complete
+					const _uploaded_url = _upload_res.data.data;
+
+					console.log(_uploaded_url);
+
+					// Updating photo
+					this.setState((prevState) => ({
+						user_profile: {
+							...prevState.user_profile,
+							photo_url: _uploaded_url
+						}
+					}));
+				} catch (error) {
+					// If uploading failed
+					// @TODO show an modal error / error message
+				}
+			}
+		});
+	}
+
 	componentDidMount() {
 		this.fetchUserCredential();
 	}
@@ -112,7 +200,7 @@ export default class Profile extends Component {
 	// ─── CALLBACK ───────────────────────────────────────────────────────────────────
 	//
 	onEditCredential(value, key) {
-		console.log(key + ' ' + value);
+		//console.log(key + ' ' + value);
 		this.setState((prevState) => ({
 			staged_edit_data: {
 				...prevState.staged_edit_data,
@@ -123,10 +211,15 @@ export default class Profile extends Component {
 	// ────────────────────────────────────────────────────────────────────────────────
 
 	render() {
-		const rendered_profile_image =
-			this.state.user_profile.photo_url ||
-			'https://cdn5.vectorstock.com/i/thumb-large/45/79/male-avatar-profile-picture-silhouette-light-vector-4684579.jpg';
+		// Rendering the image url
+		// Also generate a random string for disabling react-image-cached
+		const rendered_profile_image = this.state.user_profile.photo_url
+			? !this.state.toggleState
+				? `${this.state.user_profile.photo_url}#${new Date().toISOString()}`
+				: this.state.user_profile.photo_url
+			: 'https://cdn5.vectorstock.com/i/thumb-large/45/79/male-avatar-profile-picture-silhouette-light-vector-4684579.jpg';
 
+		console.log(rendered_profile_image);
 		return (
 			<KeyboardAvoidingView style={styles.container} behavior="height" enabled>
 				<ScrollView>
@@ -138,7 +231,7 @@ export default class Profile extends Component {
 							colors={[ '#6846ff', '#56ffd5' ]}
 							style={[ styles.cardForm, { justifyContent: 'center' } ]}
 						>
-							<View style={styles.cardBar}>
+							<View style={styles.cardBar} onStartShouldSetResponder={this.onSelectImageFromLibrary}>
 								<Image
 									//source={require('../../assets/images/user.png')}
 									source={{ uri: rendered_profile_image }}
