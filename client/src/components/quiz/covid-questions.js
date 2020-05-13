582d0aa60c64871ee83b787d018f5ce1bdad8335
BlueBearrii <thananan.math.acs@gmail.com>
2020-05-13 14:07:45 +0700

    :bug: [fixed] wrong question

--- client/src/components/quiz/covid-questions.js
+++ client/src/components/quiz/covid-questions.js
@@ -3,24 +3,24 @@
 //
 
 exports.MORNING_QUESTION = {
-	quiz_id: 'COVID01',
-	quiz_txt: 'คุณรู้สึกผิดปกติเกี๋ยวกับระบบทางเดินหายใจหรือไม่',
-	type: 0,
-	cycle: 1 // 1 for morning
+  quiz_id: 'COVID01',
+  quiz_txt: 'คุณรู้สึกผิดปกติเกี่ยวกับระบบทางเดินหายใจหรือไม่',
+  type: 0,
+  cycle: 1, // 1 for morning
 };
 
 exports.MIDDAY_QUESTION = {
-	quiz_id: 'COVID02',
-	quiz_txt: 'ก่อนหน้านี้คุณรู้สึกมีไข้บ้างหรือไม่',
-	type: 0,
-	cycle: 2 // midday
+  quiz_id: 'COVID02',
+  quiz_txt: 'ก่อนหน้านี้คุณรู้สึกมีไข้บ้างหรือไม่',
+  type: 0,
+  cycle: 2, // midday
 };
 
 exports.EVENING_QUESTION = {
-	quiz_id: 'COVID03',
-	quiz_txt: 'วันนี้คุณมีอาการไอบ้างหรือไม่',
-	type: 0,
-	cycle: 3 // evening
+  quiz_id: 'COVID03',
+  quiz_txt: 'วันนี้คุณมีอาการไอบ้างหรือไม่',
+  type: 0,
+  cycle: 3, // evening
 };
 
 // ────────────────────────────────────────────────────────────────────────────────
